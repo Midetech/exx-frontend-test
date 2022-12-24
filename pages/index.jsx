9b8ec8dac2e36e421c1d7261e954c4175db754fc
Midecodes <faginorish@gmail.com>
2022-12-24 23:12:26 +0100

    Add medium screen breakpoint

--- pages/index.jsx
+++ pages/index.jsx
@@ -33,19 +33,19 @@ export default function Home() {
     <Layout handleClick={handleConnectWallet}>
       <div className=" flex justify-center flex-col items-center gap-y-10">
         <div className="flex justify-center flex-col items-center ">
-          <p className="text-center text-[#111315] opacity-[1] lg:text-[44px] font-SpaceGrotesk lg:leading-[94px] font-[700] lg:h-[88px] lg:w-[838px] h-[153px] w-[268px] text-[40px] leading-[51px]">
+          <p className="text-center text-[#111315] opacity-[1]  md:h-auto  lg:text-[44px] font-SpaceGrotesk lg:leading-[94px] font-[700] lg:h-[88px] lg:w-[838px]  md:w-full h-[153px] w-[268px] text-[40px] leading-[51px]">
             Start building apps with{" "}
             <span className="text-[#174AFF]">useful cases</span>
           </p>
-          <p className="lg:h-[59px] lg:w-[489px] text-center lg:text-[18px] lg:leading-[23px] text-[#000] opacity-[0.6] font-DmSans h-[72px] w-[316px] text-[16px] leading-[150%] mt-3 lg:mt-0">
+          <p className="lg:h-[59px] lg:w-[489px] md:w-[375px]  md:h-auto text-center lg:text-[18px] lg:leading-[23px] text-[#000] opacity-[0.6] font-DmSans h-[72px] w-[316px] text-[16px] leading-[150%] mt-3 lg:mt-0">
             Welcome to the EXX Developer’s Guide. Now you can begin building
             your first DApp on EXX.
           </p>
-          <div className="space-y-4 lg:space-y-0 lg:flex gap-x-4 w-full lg:w-auto mt-[42px] lg:mt-[12px]">
-            <button className="  bg-[#174AFF] text-white outline-none px-6 min-w-[112px] lg:min-w-[250px] h-[54px] flex justify-center items-center gap-x-3 rounded-[10px] hover:bg-opacity-90 transition-opacity ease-linear delay-150 bg-opacity-100 cursor-pointer w-full lg:w-fit lg:px-10 border border-[#174AFF] bg-transparenttext-white">
+          <div className="space-y-4 lg:space-y-0 lg:flex md:flex md:justify-center gap-x-4 w-full lg:w-auto mt-[42px] lg:mt-[12px] md:flex-row md:w-full md:space-y-1">
+            <button className="  bg-[#174AFF] text-white outline-none px-6 min-w-[112px] lg:min-w-[250px] h-[54px] flex justify-center items-center gap-x-3 rounded-[10px] hover:bg-opacity-90 transition-opacity ease-linear delay-150 bg-opacity-100 cursor-pointer w-full  md:w-1/3 lg:w-fit lg:px-10 border border-[#174AFF] bg-transparenttext-white">
               Get Started
             </button>
-            <button className="text-[#0077FE] outline-none px-6 min-w-[112px] lg:min-w-[250px] h-[54px] flex justify-center items-center gap-x-3 rounded-[10px] hover:bg-opacity-90 transition-opacity ease-linear delay-150 bg-opacity-100 cursor-pointer w-full lg:w-fit lg:px-10 border border-[#174AFF] bg-transparent">
+            <button className="text-[#0077FE] outline-none px-6 min-w-[112px] lg:min-w-[250px]  md:w-1/3  h-[54px] flex justify-center items-center gap-x-3 rounded-[10px] hover:bg-opacity-90 transition-opacity ease-linear delay-150 bg-opacity-100 cursor-pointer w-full lg:w-fit lg:px-10 border border-[#174AFF] bg-transparent">
               <p>Faucet</p>
               <span>
                 <svg
@@ -111,12 +111,12 @@ export default function Home() {
           </div>
 
           <div className="lg:absolute lg:-bottom-[19rem] bg-[#174AFF] lg:h-[512px] h-[425px] px-5 w-screen flex flex-col items-center relative">
-            <p className="text-white lg:w-[937px] lg:text-[44px] font-SpaceGrotesk lg:leading-[54px] lg:mt-[51px] text-[32px] leading-[41px] w-[317px] pt-10">
+            <p className="text-white lg:w-[937px] md:w-full md:text-center lg:text-[44px] font-SpaceGrotesk lg:leading-[54px] lg:mt-[51px] md:mt-[40px] text-[32px] leading-[41px] w-[317px] pt-10">
               Your journey to developing your first dapp.
             </p>
             <div
               style={{ boxShadow: "0px 4px 42px rgba(0, 0, 0, 0.1)" }}
-              className="lg:absolute lg:-bottom-10 mt-10 bg-white lg:h-[332px] h-[709px] lg:w-[1259px] w-[335px] rounded-[19px]  flex justify-between items-center flex-col lg:flex-row p-5"
+              className="lg:absolute md:absolute lg:-bottom-10  md:-bottom-8 mt-10 bg-white lg:h-[332px] h-[709px]  md:h-auto   lg:w-[1259px] md:w-[750px] w-[335px] rounded-[19px]  flex justify-between items-center flex-co md:flex-row lg:flex-row p-5"
             >
               {steps.map((item, index) => (
                 <div
@@ -138,16 +138,37 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex lg:mt-[380px] mt-[450px]">
-          <div className="lg:bg-[#f6f6f6] lg:h-[354px] lg:w-[1130px] rounded-[20px] flex lg:flex-row flex-col gap-5 justify-around lg:items-center">
-            <div className="lg:w-[402px] lg:h-[221px]">
+        <div className="flex lg:mt-[380px] mt-[450px] md:mt-[50px]">
+          <div className="lg:bg-[#f6f6f6] lg:h-[354px] lg:w-[1130px] rounded-[20px] flex lg:flex-row md:flex-row flex-col gap-5 justify-around lg:items-center">
+            <div className="lg:w-[402px] md:w-1/2 lg:h-[221px]">
               <p className="lg:w-[373px] lg:h-[108px] text-[44px] leading-[54px] text-[#000] font-SpaceGrotesk font-bold mb-10">
                 Get connected to the EXX Network
               </p>
               {errMessage && (
                 <p className="text-red-600 text-sm font-DmSans">{errMessage}</p>
               )}
-              <div className="space-y-4 lg:space-y-0 lg:flex gap-x-4 w-full lg:w-auto">
+
+              <div className=" lg:hidden md:hidden lg:w-[430px] lg:h-[258px] flex flex-col justify-between mb-[29px]">
+                <p className="lg:text-[18px] font-DmSans leading-[30px] text-[#000]">
+                  <span className="opacity-[0.6]">PRC URL:</span>{" "}
+                  https://ds2.exx.network
+                </p>
+                <p className="lg:text-[18px] font-DmSans leading-[30px] text-[#000]">
+                  <span className="opacity-[0.6]"> Network name:</span> Exx
+                  Testnet
+                </p>
+                <p className="lg:text-[18px] font-DmSans leading-[30px] text-[#000]">
+                  <span className="opacity-[0.6]">Symbol: </span> EXX
+                </p>
+                <p className="lg:text-[18px] font-DmSans leading-[30px] text-[#000]">
+                  <span className="opacity-[0.6]"> Chain ID:</span> 47
+                </p>
+                <p className="lg:text-[18px] font-DmSans leading-[30px] text-[#000]">
+                  <span className="opacity-[0.6]"> Block Explorer: </span>
+                  https://exxscan.com
+                </p>
+              </div>
+              <div className="space-y-4 lg:space-y-0 lg:flex gap-x-4 w-full lg:w-auto md:flex md:flex-row md:items-center md:space-y-0">
                 <button
                   onClick={handleConnectWallet}
                   className=" text-[12px] bg-[#174AFF] text-white outline-none px-6 min-w-[112px] lg:min-w-[192px] h-[54px] flex justify-center items-center gap-x-3 rounded-[10px] hover:bg-opacity-90 transition-opacity ease-linear delay-150 bg-opacity-100 cursor-pointer w-full lg:w-fit lg:px-10 border border-[#174AFF] bg-transparenttext-white"
@@ -160,7 +181,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="lg:w-[430px] lg:h-[258px] flex flex-col justify-between ">
+            <div className="lg:w-[430px] lg:h-[258px] lg:flex md:flex hidden flex-col justify-between ">
               <p className="lg:text-[18px] font-DmSans leading-[30px] text-[#000]">
                 <span className="opacity-[0.6]">PRC URL:</span>{" "}
                 https://ds2.exx.network
@@ -182,19 +203,22 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="flex justify-between lg:flex-row flex-col lg:my-20 w-full gap-y-5 lg:w-[1145px]">
+        <div className="flex justify-between lg:flex-row  md:flex-row flex-col lg:my-20 md:my-10 md:gap-x-3 w-full gap-y-5 lg:w-[1145px] md:w-full">
           {resources.map((item, index) => (
             <div
               key={index}
-              className="lg:w-[386px] lg:h-[110px] flex flex-col justify-between  "
+              className="lg:w-[386px] md:w-1/3 lg:h-[110px] flex flex-col justify-between  "
             >
-              <p className="font-WorkSans font-bold text-[24px] leading-[125%] text-[#2A6CF9B5]  opacity-[0.7]">{`0${
+              <p className="lg:block md:block hidden font-WorkSans font-bold text-[24px] leading-[125%] text-[#174AFF]  opacity-[0.7]">{`0${
                 index + 1
               }`}</p>
-              <p className="font-SpaceGrotesk font-bold lg:text-[24px] lg:leading-[125%] text-[#2c3131]">
+              <p className="font-SpaceGrotesk font-bold lg:text-[24px] lg:leading-[125%] text-[#2c3131] text-[20px]">
+                <span className="lg:hidden  md:hidden font-WorkSans font-bold text-[24px] leading-[125%] text-[#174AFF]">{`0${
+                  index + 1
+                }`}</span>{" "}
                 {item.title}
               </p>
-              <p className="text-[#626262] text-[18px] leading-[30px] font-DmSans">
+              <p className="text-[#626262] text-[18px] leading-[30px] font-DmSans md:w-[90%]">
                 {item.text}
               </p>
               <a
@@ -208,8 +232,8 @@ export default function Home() {
         </div>
 
         <div>
-          <div className="lg:w-[1129px] lg:h-[460px] h-[707px] w-[90vw] bg-[#000] rounded-[24px] lg:relative overflow-hidden flex lg:items-center ">
-            <div className="absolute z-50 left-2 text-white lg:p-10  py-8 px-12 flex flex-col justify-center gap-y-5 lg:gap-y-10 lg:w-[535px]">
+          <div className="lg:w-[1129px] md:w-[95vw] md:flex-row lg:h-[460px] h-[707px] w-[90vw] bg-[#000] rounded-[24px] lg:relative md:relative  md:flex  overflow-hidden flex lg:items-center ">
+            <div className="absolute z-50 left-2 text-white lg:p-10  py-8 px-12 flex flex-col  justify-center gap-y-5 lg:gap-y-10 lg:w-[535px] md:w-1/2">
               <p className="font-SpaceGrotesk font-bold lg:text-[56px] leading-[103.1%] lg:h-[116px] lg:w-[535px] text-[36px] w-[239px] h-[111px] ">
                 Join our developer community
               </p>
@@ -223,12 +247,21 @@ export default function Home() {
                 <p>Join the community</p>
               </button>
             </div>
-            <div className="lg:w-[1129px] lg:h-[460px] relative h-[606px]  w-full">
-              <div className="lg:w-[1228px] lg:h-[1228px] bg-[#fff] bg-opacity-[0.05] rounded-full absolute lg:left-[400px] lg:top-[-248px] w-[606px] h-[606px] left-[-24px] top-[331px]"></div>
-              <div className="lg:w-[766px] lg:h-[766px] bg-[#174AFF] rounded-full absolute lg:left-[631px] lg:top-[-17px]  w-[392px] h-[392px] left-[83px] top-[438px]"></div>
+            <div className="lg:w-[1129px] md:w-1/2 lg:h-[460px] relative h-[606px] w-full">
+              <div className="lg:w-[1228px] md:w-1/2 lg:h-[1228px] bg-[#fff] bg-opacity-[0.05] rounded-full absolute lg:left-[400px] lg:top-[-248px] w-[606px] h-[606px] left-[-24px] top-[331px]"></div>
+              <div className="lg:w-[766px]  md:w-1/2lg:h-[766px] bg-[#174AFF] rounded-full absolute lg:left-[631px] lg:top-[-17px]  w-[392px] h-[392px] left-[83px] top-[438px]"></div>
               <div className="lg:w-[536px] lg:h-[536px] bg-[#C2F3FF] bg-opacity-[0.2] rounded-full absolute lg:left-[724px] lg:top-[98px]  w-[274.3px] h-[274.3px] left-[150.59px] top-[506.85px]"></div>
-              <div className="lg:w-[356.83px] lg:h-[356.83px] bg-[#E2F8FF] rounded-full absolute lg:left-[803.9px] lg:top-[180.65px]  w-[182.61px] h-[182.61px] -right-4 top-[559.15px]"></div>
-
+              <div className="lg:w-[356.83px] md:w-1/2 lg:h-[356.83px] bg-[#E2F8FF] rounded-full absolute lg:left-[803.9px] lg:top-[180.65px]  w-[182.61px] h-[182.61px] -right-4 top-[559.15px]"></div>
+              <img
+                src="/assets/code.svg"
+                alt=""
+                className="absolute lg:left-[714px] lg:top-[113px]  left-[145.48px] top-[518.53px] w-65.51px] h-[43.31px] lg:w-[128.02px] lg:h-[84.62px]"
+              />
+              <img
+                src="/assets/blockchain-developer.svg"
+                alt=""
+                className="absolute lg:left-[952px] lg:top-[49px] left-[267.27px] top-[481.78px] w-[49.99px] h-[40.99px] lg:w-[80.11px] lg:h-[80.11px]"
+              />
               <div className="absolute right-0 lg:bottom-0 bottom-[-5.5rem] z-50">
                 <svg
                   width="294"
@@ -272,7 +305,7 @@ export default function Home() {
           <div className="flex justify-between flex-col lg:flex-row gap-5">
             {news.map((item, index) => (
               <div
-                className="lg:w-[400px] lg:h-[431px] bg-[#f5f5f5] rounded-[20px]  flex flex-col justify-between"
+                className="lg:w-[400px] lg:h-[431px] lg:bg-[#f5f5f5] bg-[#F0F9FF] rounded-[20px]  flex flex-col justify-between"
                 key={index}
               >
                 <div className="flex justify-between   p-4">
@@ -292,7 +325,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="w-screen bg-black  lg:h-[327px] lg:my-14 flex lg:flex-row flex-col h-[278px] justify-center lg:items-center  lg:gap-[145px]  gap-[14px]">
+        <div className="w-screen bg-black bg-[url(/assets/vector.svg)] bg-no-repeat bg-contain lg:h-[327px] lg:my-14 flex lg:flex-row  md:flex-row flex-col h-[278px] justify-center md:justify-between md:items-center lg:items-center  lg:gap-[145px]  gap-[14px]">
           <div className="px-4">
             <p className="lg:w-[475.5px] lg:h-[80px] lg:text-[44px] lg:leading-[80px] text-[24px] leading-[24px] font-SpaceGrotesk font-bold  text-white">
               Stay Updated
@@ -302,14 +335,14 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="flex gap-[14px]  lg:flex-row flex-col  px-4">
+          <div className="flex gap-[14px]  lg:flex-row flex-col md:flex-row px-4">
             <input
               placeholder="Your email address"
               type="email"
               className="lg:w-[456px] h-[54px] w-full rounded-[10px] border border-[rgba(255, 255, 255, 0.48)] bg-transparent p-2"
             />
 
-            <button className="outline-none p-[16.633px 24.95px] flex justify-center  items-center flex-row gap-[8.32px]  bg-[#174AFF] rounded-[10px] border border-[#174AFF] lg:w-[167px] h-[54px] text-white font-DmSans font-bold text-[14px]  leading-[18px]">
+            <button className="outline-none p-[16.633px 24.95px] md:w-[219px] flex justify-center  items-center flex-row gap-[8.32px]  bg-[#174AFF] rounded-[10px] border border-[#174AFF] lg:w-[167px] h-[54px] text-white font-DmSans font-bold text-[14px]  leading-[18px]  md:text-xs">
               <p>Subscribe now</p>
               <img src="/assets/arrow.svg" alt="exx network" />
             </button>
--- shared/Footer/index.jsx
+++ shared/Footer/index.jsx
@@ -3,91 +3,115 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="  font-SpaceGrotesk overflow-hidden lg:bg-white flex  justify-center py-7 px-5 lg:py-7 lg:px-7 xl:py-6 xl:px-20 3xl:bg-transparent flex-col lg:flex-row w-full">
-      <div className="flex flex-col gap-y-4 pb-5">
-        <img
-          className="w-[99px] lg:w-28 lg:hidden block "
-          src="/assets/exx-logo.svg"
-          alt="exx network"
-        />
-        <p className="hidden lg:block text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
-          EXX Blockchain
-        </p>
-        <p className="lg:w-[330px] lg:h-[54px] font-[400] lg:text-[18px] text-[14px] leading-[150%] text-[#596780]  font-DmSans w-[327px]">
-          EXX builds on the efficacy of Ethereum, it is faster, powerful and
-          more secured
-        </p>
-      </div>{" "}
-      <div className="flex flex-row  flex-wrap lg:flex-nowrap justify-center lg:gap-x-[120px]  gap-y-[40px] w-full">
-        <div className="flex flex-col gap-y-4 w-[50%] lg:w-auto">
-          <p className="text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
-            General
+    <div className="  font-SpaceGrotesk overflow-hidden lg:bg-white flex justify-center py-7 px-5 lg:py-7 lg:px-7 xl:py-6 xl:px-20 3xl:bg-transparent flex-col  w-full">
+      <div className="flex  justify-center lg:flex-row flex-col">
+        <div className="flex flex-col gap-y-4 pb-5">
+          <img
+            className="w-[99px] lg:w-28 lg:hidden block "
+            src="/assets/exx-logo.svg"
+            alt="exx network"
+          />
+          <p className="hidden lg:block text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
+            EXX Blockchain
           </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            About
+          <p className="lg:w-[330px] lg:h-[54px] font-[400] lg:text-[18px] text-[14px] leading-[150%] text-[#596780]  font-DmSans w-[327px]">
+            EXX builds on the efficacy of Ethereum, it is faster, powerful and
+            more secured
           </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Contact us
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Brand Kits
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            New & Updates
-          </p>
-        </div>
-        <div className="flex flex-col gap-y-4 w-[50%] lg:w-auto">
-          <p className="text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
-            Explore
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Documentation
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Block Explorer
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Github
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Community
-          </p>
-        </div>
-        <div className="flex flex-col gap-y-4 w-[50%] lg:w-auto">
-          <p className="text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
-            Apply
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            {" "}
-            Dev. Incubator{" "}
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Career
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Ambassador
-          </p>
-        </div>
-        <div className="flex flex-col gap-y-4 w-[50%] lg:w-auto">
-          <p className="text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
-            Follow Us
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            {" "}
-            Twitter{" "}
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Medium
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Telegram
-          </p>
-          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
-            Discord
-          </p>
+        </div>{" "}
+        <div className="flex flex-row  flex-wrap lg:flex-nowrap justify-center lg:gap-x-[120px]  gap-y-[40px] w-full">
+          <div className="flex flex-col gap-y-4 w-[50%] lg:w-auto  md:w-1/4">
+            <p className="text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
+              General
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              About
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Contact us
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Brand Kits
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              New & Updates
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-4 w-[50%] lg:w-auto  md:w-1/4">
+            <p className="text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
+              Explore
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Documentation
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Block Explorer
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Github
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Community
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-4 w-[50%] lg:w-auto  md:w-1/4">
+            <p className="text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
+              Apply
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              {" "}
+              Dev. Incubator{" "}
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Career
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Ambassador
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-4 w-[50%] lg:w-auto  md:w-1/4">
+            <p className="text-[#1D2A65] lg:text-[24px] leading-[31px] font-[700]">
+              Follow Us
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              {" "}
+              Twitter{" "}
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Medium
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Telegram
+            </p>
+            <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+              Discord
+            </p>
+          </div>
         </div>
       </div>
-      <div class="flex-1 my-5 border-t border-[#0000001C] w-full"></div>
+      <div class="my-5 border-t border-[rgba(23, 74, 255, 0.13)] w-full  lg:mt-[40px]"></div>
+
+      <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start gap-[22px]">
+        <div className="flex items-center lg:gap-[32px] gap-[20px]">
+          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+            Privacy Policy
+          </p>
+
+          <div className="border-r border-[rgba(23, 74, 255, 0.13)] hidden lg:block h-[27px]"></div>
+          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+            Terms & Conditions
+          </p>
+
+          <div className="border-r border-[rgba(23, 74, 255, 0.13)] hidden lg:block h-[27px]"></div>
+          <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+            Disclaimer
+          </p>
+        </div>
+
+        <p className="text-[16px] leading-[150%] text-[#596780] text-justify font-[400] font-DmSans">
+          With love 💙 from Exxample, Inc
+        </p>
+      </div>
     </div>
   );
 };
